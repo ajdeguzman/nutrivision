be72717b1f4d2e8795b342cef0229d460dab6821
ajdeguzman <aljohndeguzman@gmail.com>
2016-11-18 18:54:20 +0800

    started recent searches

--- app/src/main/java/edu/ucuccs/nutrivision/PrefManager.java
+++ app/src/main/java/edu/ucuccs/nutrivision/PrefManager.java
@@ -1,37 +1,56 @@
+/**
+ * Author: UCU Knight Coders on 11/12/2016.
+ * Website: http://facebook.com/teamucuccs
+ * E-mail: teamucuccs@gmail.com
+ */
+
 package edu.ucuccs.nutrivision;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 
-/**
- * Created by Vin on 11/12/2016.
- */
+import java.util.ArrayList;
+import java.util.Map;
 
-public class PrefManager {
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    Context _context;
 
-    // shared pref mode
-    int PRIVATE_MODE = 0;
+class PrefManager {
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private SharedPreferences prefs_recent;
+    private static final String PREF_RECENT_SEARCHES = "prefs_recent";
+    private static final String PREF_NAME = "prefs_intro";
+    private static final String IS_FIRST_TIME_LAUNCH = "prefs_first_time_launch";
 
-    // Shared preferences file name
-    private static final String PREF_NAME = "nutrivision-intro";
-
-    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
-
-    public PrefManager(Context context) {
-        this._context = context;
-        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+    PrefManager(Context context) {
+        int PRIVATE_MODE = 0;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        prefs_recent = context.getSharedPreferences(PREF_RECENT_SEARCHES, Context.MODE_PRIVATE);
         editor = pref.edit();
     }
 
-    public void setFirstTimeLaunch(boolean isFirstTime) {
+    void setFirstTimeLaunch(boolean isFirstTime) {
         editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
         editor.commit();
     }
 
-    public boolean isFirstTimeLaunch() {
+    boolean isFirstTimeLaunch() {
         return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    /*
+    * Preferences for Food recent searches
+     */
+    void insertRecentItem(String keyword) {
+        prefs_recent.edit().putString(keyword, keyword).commit();
+    }
+
+    ArrayList getAllRecentSearches() {
+        Map<String,?> keys = prefs_recent.getAll();
+        ArrayList<Integer> arrRecentItems = new ArrayList<>();
+
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            arrRecentItems.add(Integer.parseInt(entry.getKey()));
+        }
+        return arrRecentItems;
     }
 }
